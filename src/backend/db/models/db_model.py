19149c21a22c2860ee89aa778c381b9507a391d4
junxiaoliao <huruyuanxingke66@gmail.com>
2024-03-03 00:33:43 +0800

    create orm models

---
+++ src/backend/db/models/db_model.py
@@ -0,0 +1,38 @@
+from sqlalchemy import Column, Integer, String, JSON, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy_utils import URLType
+
+from backend.db.models.utils.mixins import Timestamp 
+from backend.db.db_setup import Base
+
+
+# Image SQLAlchemy ORM Model
+class Image(Timestamp, Base):
+    __tablename__ = "image"
+    id = Column(Integer, primary_key=True)
+    file_key = Column(URLType, nullable=False)
+    document_id = Column(Integer, ForeignKey("document.id"), nullable=False)
+
+    document = relationship("Document", back_populates="images")
+
+
+# Document SQLAlchemy ORM Model
+class Document(Timestamp, Base):
+    __tablename__ = "document"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    ocr_result = Column(JSON, nullable=False)  # Using JSON format to store OCR result
+    category_id = Column(Integer, ForeignKey("category.id"), nullable=False)
+
+    images = relationship("Image", back_populates="document")
+    category = relationship("Category", back_populates="documents")
+
+
+# Category SQLAlchemy ORM Model
+class Category(Timestamp, Base):
+    __tablename__ = "category"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    description = Column(String, nullable=True)
+
+    documents = relationship("Document", back_populates="category")
