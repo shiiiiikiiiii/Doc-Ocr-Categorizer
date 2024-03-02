9106261cb6b0702b310ba99842b12fad8c6c9943
junxiaoliao <huruyuanxingke66@gmail.com>
2024-03-02 23:41:56 +0800

    update db config

--- src/backend/db/db_setup.py
+++ src/backend/db/db_setup.py
@@ -1,19 +1,12 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://gwen@localhost/fast_lms"
-ASYNC_SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://gwen@localhost/fast_lms"
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres@localhost/postgres"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={}, future=True)
-async_engine = create_async_engine(ASYNC_SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(
-    autocommit=False, autoflush=False, bind=engine, future=True
-)
-AsyncSessionLocal = sessionmaker(
-    async_engine, class_=AsyncSession, expire_on_commit=False
-)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, future=True)
 
 Base = declarative_base()
 
@@ -25,9 +18,3 @@ def get_db():
         yield db
     finally:
         db.close()
-
-
-async def async_get_db():
-    async with AsyncSessionLocal() as db:
-        yield db
-        await db.commit()
