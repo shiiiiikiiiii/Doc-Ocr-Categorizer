edd02693ad9a12a49e0307432e95937ae39f79c7
junxiaoliao <huruyuanxingke66@gmail.com>
2024-03-19 18:00:05 +0800

    update CORS support

--- src/backend/main.py
+++ src/backend/main.py
@@ -3,9 +3,22 @@ from fastapi import FastAPI
 from api import document, category
 from db.db_setup import engine, Base
 
+from fastapi.middleware.cors import CORSMiddleware
+
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:8000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(document.router)
 app.include_router(category.router)
