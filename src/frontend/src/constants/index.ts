a8caa233b9a404411dc5744fda0e55107dd88b0b
junxiaoliao <huruyuanxingke66@gmail.com>
2024-03-27 17:51:03 +0800

    delete unused umi generated files

--- src/frontend/src/constants/index.ts
+++
@@ -1,1 +0,0 @@
-export const DEFAULT_NAME = 'Doc-Ocr-Categorizer';
--- src/frontend/src/pages/Home/index.tsx
+++ src/frontend/src/pages/Home/index.tsx
@@ -1,18 +1,11 @@
-import Guide from '@/components/Guide';
-import { trim } from '@/utils/format';
-import { PageContainer } from '@ant-design/pro-components';
-import { useModel } from '@umijs/max';
-import styles from './index.less';
+import React from 'react';
+import { Typography } from 'antd';
+const { Title } = Typography;
 
-const HomePage: React.FC = () => {
-  const { name } = useModel('global');
-  return (
-    <PageContainer ghost>
-      <div className={styles.container}>
-        <Guide name={trim(name)} />
-      </div>
-    </PageContainer>
-  );
-};
+const HomePage = () => (
+  <>
+    <Title>Welcome to Doc-Ocr-Categorizer</Title>
+  </>
+);
 
 export default HomePage;
